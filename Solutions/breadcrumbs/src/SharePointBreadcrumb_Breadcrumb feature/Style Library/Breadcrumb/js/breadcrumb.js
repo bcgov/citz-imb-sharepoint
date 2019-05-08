86e18195c6bf528f671dc1fd41987ba8d9f5af57
SDToews <33942967+SDToews@users.noreply.github.com>
2019-05-08 07:52:54 -0700

    Ready for Review

--- Solutions/breadcrumbs/src/SharePointBreadcrumb_Breadcrumb feature/Style Library/Breadcrumb/js/breadcrumb.js
+++ Solutions/breadcrumbs/src/SharePointBreadcrumb_Breadcrumb feature/Style Library/Breadcrumb/js/breadcrumb.js
@@ -1,202 +1,181 @@
+/*
+    Creates a breadcrumb trail for all pages in the collection
+    Version 1.0
+*/
 var breadcrumbProperties = {
-    showFolders: true,
-    breadcrumbElement: "",
-    pagesLibrary: "Pages",
-    sitePagesLibrary: "Site Pages",
-    hidePageLibraries: true,
-    hideFromPages: [""]
+    ExcludedLists: ["Site Pages", "Pages"]
 };
 
-document.addEventListener("DOMContentLoaded", function () {
+function getSiteInfo(sitesArray) {
+    var defer = jQuery.Deferred();
 
-    for (var i = 0; i < breadcrumbProperties.hideFromPages.length; i++) {
-        if (document.location.href == breadcrumbProperties.hideFromPages[i]) {
-            return;
-        }
-    }
+    var current = sitesArray.shift();
 
+    if (sitesArray.length === 0) {
+        return "<span class='csp_breadcrumb'><a href='" + _spPageContextInfo.webAbsoluteUrl + "' title=''>" + _spPageContextInfo.webTitle + "</a></span>";
+    } else {
+        SP.SOD.executeFunc("SP.js", "SP.ClientContext", function () {
+            var currentSite = _spPageContextInfo.webAbsoluteUrl.substring(0, _spPageContextInfo.webAbsoluteUrl.search(current) + current.length)
+            var ctx = new SP.ClientContext(currentSite);
+            var oSite = ctx.get_web();
 
-//Get script location to inject the css file
-    var scripts = document.getElementsByTagName("script");
-    var pathBreadCrumb = "";
-    if (scripts && scripts.length > 0) {
-        for (var i in scripts) {
-            if (scripts[i].src && scripts[i].src.match(/\/js\/breadcrumb\.js/)) {
-				pathBreadCrumb = scripts[i].src.replace(/(.*)\/js\/breadcrumb\.js.*/,"$1/");
-                break;
-            }
-        }
-    };
+            ctx.load(oSite, "Title", "Description");
 
-    function injectCSS() {
-        try {
-            if (pathBreadCrumb) {
-                var style = document.createElement("link");
-                style.href = pathBreadCrumb + 'css/breadcrumb.css';
-                style.type = "text/css";
-                style.rel = "stylesheet";
-                document.getElementsByTagName("head")[0].appendChild(style);
-            }
-        }
-        catch (ex) {
-
-        }
-    }
-
-    injectCSS();
-
-
-    SP.SOD.executeFunc('SP.js', 'SP.ClientContext', LoadSiteBreadcrumb);
-
-    function getListTitle() {
-        var listTile = _spPageContextInfo.listTitle;
-        var listUrl = _spPageContextInfo.listUrl;
-
-        //not supported on SharePoint on prem
-        if (listTile == undefined) {
-            return;
-        }
-
-        if (!breadcrumbProperties.hidePageLibraries) {
-            if ((listTile == breadcrumbProperties.sitePagesLibrary) || (listTile == breadcrumbProperties.pagesLibrary)) {
-                $('#breadcrumbSite li:last').before('<li class="ms-Breadcrumb-listItem"><a class="ms-Breadcrumb-itemLink" href="' + listUrl + '">' + listTile + '</a><i class="ms-Breadcrumb-chevron ms-Icon ms-Icon--ChevronRight"></i></li>');
-            }
-        }
-    }
-
-    function getFolders() {
-        if ($('#DeltaPlaceHolderPageTitleInTitleArea span span').length > 0 && (document.location.href).indexOf('RootFolder=') != -1) {
-            //get nodes from url
-            var siteServerRelativeUrl = _spPageContextInfo.siteServerRelativeUrl;
-            if (siteServerRelativeUrl == "/") {
-                siteServerRelativeUrl = "";
-            }
-
-            var libraryLocation = unescape(document.location).split("RootFolder=")[1].split("&Folder")[0].replace(siteServerRelativeUrl, '');
-
-            var libraryNodes = libraryLocation.split('/');
-            var libraryNodeURL = _spPageContextInfo.serverRequestPath + "?RootFolder=" + siteServerRelativeUrl;
-            $.each(libraryNodes, function (index, value) {
-
-                if (value != "") {
-                    libraryNodeURL = libraryNodeURL + "/" + value;
-                    $('#breadcrumbSite').append('<li class="ms-Breadcrumb-listItem"><a class="ms-Breadcrumb-itemLink" href="' + libraryNodeURL + '">' + value + '</a><i class="ms-Breadcrumb-chevron ms-Icon ms-Icon--ChevronRight"></i></li>');
+            ctx.executeQueryAsync(
+                function (result) {
+                    //sucess
+                    defer.resolve("<span class='csp_breadcrumb'><a href='" + currentSite + "' title='" + oSite.get_description() + "'>" + oSite.get_title() + "</a></span>" + getSiteInfo(sitesArray));
+                },
+                function (err) {
+                    //fail
+                    defer.reject("Request Failed: ", err);
 
                 }
-                
+            );
+        });
+    }
 
-            });
+    return defer.promise();
+}
+function makeSiteBreadrumb() {
+    var defer = jQuery.Deferred();
+
+    //get root site info
+    //var rootSite = _spPageContextInfo.siteServerRelativeUrl.replace("/sites/", "");
+    SP.SOD.executeFunc("SP.js", "SP.ClientContext", function () {
+        var html;
+        var ctx = new SP.ClientContext(_spPageContextInfo.siteAbsoluteUrl);
+        var oSite = ctx.get_web();
+
+        ctx.load(oSite, "Title", "Description");
+
+        ctx.executeQueryAsync(
+            function (result) {
+                //sucess
+                html = "<span class='csp_breadcrumb'><a href='" + _spPageContextInfo.siteAbsoluteUrl + "' title='" + oSite.get_description() + "'>" + oSite.get_title() + "</a></span>";
+
+                if (_spPageContextInfo.siteServerRelativeUrl !== _spPageContextInfo.webServerRelativeUrl) {
+                    var s = _spPageContextInfo.webServerRelativeUrl.replace(_spPageContextInfo.siteServerRelativeUrl, "");
+                    var a = s.split("/");
+                    a.shift();
+                    $.when(getSiteInfo(a)).then(function (data) {
+                        defer.resolve(html + data);
+                    });
+                } else {
+                    defer.resolve(html);
+                }
+            },
+            function (err) {
+                //fail
+                defer.reject(err);
+            }
+        );
+    });
+
+    return defer.promise();
+}
+
+
+function getListInfo() {
+    var defer = jQuery.Deferred();
+    if (typeof _spPageContextInfo.pageListId === "undefined") {
+        //not part of a list or library
+        defer.resolve("");
+    } else {
+
+
+        SP.SOD.executeFunc("SP.js", "SP.ClientContext", function () {
+            var html;
+            var ctx = new SP.ClientContext.get_current();
+            var oList = ctx.get_web().get_lists().getById(_spPageContextInfo.pageListId);
+            var oListRootFolder = oList.get_rootFolder();
+
+            ctx.load(oList);
+            ctx.load(oListRootFolder);
+
+            ctx.executeQueryAsync(
+                function (result) {
+                    //success
+                    if (breadcrumbProperties.ExcludedLists.includes(oList.get_title())) {
+                        html = "";
+                    } else {
+                        html = "<span class='csp_breadcrumb'><a href='" + window.location.hostname + oListRootFolder.get_serverRelativeUrl() + "' title='" + oList.get_description() + "'>" + oList.get_title() + "</a></span>";
+                    }
+                    console.log(html);
+                    defer.resolve(html);
+                },
+                function (err) {
+                    //fail
+                    defer.reject(err);
+                }
+            );
+        });
+    }
+
+    return defer.promise();
+}
+function getFolderInfo() {
+    var html = "";
+    var u = decodeURIComponent(window.location.href);
+    console.log(u);
+    if (u.search("RootFolder") > -1) {
+        if (u.search("_layouts") > -1) {
+            //not a list or a library
+            return "";
+        } else {
+
+            var v = u.split("?")[1].split("&");
+            var path;
+            var listpath = window.location.protocol + "//" + window.location.hostname + _spPageContextInfo.webServerRelativeUrl;
+            for (i = 0; i < v.length; i++) {
+                if (v[i].startsWith("RootFolder")) {
+                    path = v[i].split("=")[1].replace(_spPageContextInfo.webServerRelativeUrl, "");
+                    break;
+                }
+            }
+            if (path.startsWith("/Lists")) {
+                listpath += "/Lists";
+                path = path.substring(6);
+            }
+            var folders = path.split("/");
+            folders.shift();  //remove the first blank
+            listpath = listpath + "/" + folders.shift();  // value and the list name
+
+            for (j = 0; j < folders.length; j++) {
+                listpath = listpath + "/" + folders[j];
+                html += "<span class='csp_breadcrumb'><a href='" + listpath + "'>" + folders[j] + "</a></span>";
+            }
         }
     }
 
+    return html;
+}
+function getPageInfo() {
+    return "<span class='csp_breadcrumb'>" + _spPageContextInfo.serverRequestPath.substr(_spPageContextInfo.serverRequestPath.lastIndexOf('/') + 1).split(".")[0] + "</span>";
+}
 
-    function LoadSiteBreadcrumb() {
-        var breadCrumbNode;
-        var clientcontext = SP.ClientContext.get_current();
-        var site = clientcontext.get_site();
-        var currentWeb = clientcontext.get_web();
-        clientcontext.load(currentWeb, 'ServerRelativeUrl', 'Title', 'ParentWeb', 'Url');
-        clientcontext.load(site, 'ServerRelativeUrl');
-        clientcontext.executeQueryAsync(
-		function () {
-		    var breadcrumbWrapper = document.createElement('div');
-		    breadcrumbWrapper.className = "ms-Breadcrumb";
-		    breadcrumbWrapper.innerHTML = '<div class="ms-FocusZone"><ul id="breadcrumbSite" class="ms-Breadcrumb-list"></ul></div>';
+$().ready(function () {
+    $("#pageTitle").addClass("ms-hidden");
+    //put the css reference on the page
+    var cssUrl = "https://" + window.location.hostname + _spPageContextInfo.siteServerRelativeUrl + "/Style%20Library/Breadcrumb/css/breadcrumb.css";
+    var head = document.getElementsByTagName("head")[0];
+    var style = document.createElement("link");
+    style.type = "text/css";
+    style.rel = "stylesheet";
+    style.href = cssUrl;
+    head.appendChild(style);
 
-		    //for Seattle Master
-		    if (breadcrumbProperties.breadcrumbElement == "") {
-		        if (document.getElementById('contentRow') !== null) {
-		            try {
-		                //old Seattle master
-		                document.getElementById('DeltaPlaceHolderMain').insertBefore(breadcrumbWrapper, document.getElementById('mainContent'));
-		            } catch (err) {
-		                document.getElementById('contentBox').insertBefore(breadcrumbWrapper, document.getElementById('DeltaPlaceHolderMain'));
-		            }
-		        } else {
-		            document.getElementById('contentBox').insertBefore(breadcrumbWrapper, document.getElementById('DeltaPlaceHolderMain'));
-		        }
-		    } else {
-		        $(breadcrumbProperties.breadcrumbElement).append(breadcrumbWrapper);
-		    }
+    //make the breadcrumb trail
+    //get the site(s)
+    var bcSites = makeSiteBreadrumb();
+    var bcList = getListInfo();
 
+    $.when(bcSites, bcList).then(function (siteData, listData) {
+        var bcFolders = getFolderInfo();
+        var bcPage = getPageInfo(_spPageContextInfo.pageItemId);
 
-		    var breadCrumbNode = document.getElementById('breadcrumbSite');
-		    var Custombreadcrumb = document.getElementById('DeltaPlaceHolderMain');
-		    var breadCrumbNode = document.getElementById('breadcrumbSite');
-		    if (document.location.pathname.indexOf(breadcrumbProperties.sitePagesLibrary) != -1 || document.location.pathname.indexOf(breadcrumbProperties.pagesLibrary) != -1) {
-		        var li = document.createElement('li');
-		        li.className = "ms-Breadcrumb-listItem";
-		        if (document.title.split('-').length > 1) {
-		            li.innerHTML = '<span class="ms-Breadcrumb-itemLink">' + document.title.split('-')[1].trim() + '</span><i class="ms-Breadcrumb-chevron ms-Icon ms-Icon--ChevronRight"></i>'
-		        } else {
-		            li.innerHTML = '<span class="ms-Breadcrumb-itemLink">' + document.title.trim() + '</span><i class="ms-Breadcrumb-chevron ms-Icon ms-Icon--ChevronRight"></i>'
-		        }
-		        breadCrumbNode.insertBefore(li, breadCrumbNode.childNodes[0]);
-		    }
-		    else if (document.location.pathname.indexOf('_layouts/15/') != -1) {
-		        var li = document.createElement('li');
-		        li.className = "ms-Breadcrumb-listItem";
-		        li.innerHTML = '<span class="ms-Breadcrumb-itemLink">' + document.title + '</span><i class="ms-Breadcrumb-chevron ms-Icon ms-Icon--ChevronRight"></i>'
-		        breadCrumbNode.insertBefore(li, breadCrumbNode.childNodes[0]);
-		    }
+        var html = "<div id='csp_breadcrumbContainer'>" + siteData + listData + bcFolders + bcPage + "</div>";
 
-		    var li = document.createElement('li');
-		    li.className = "ms-Breadcrumb-listItem";
-		    li.innerHTML = '<a class="ms-Breadcrumb-itemLink" href="' + currentWeb.get_url() + '">' + currentWeb.get_title() + '</a><i class="ms-Breadcrumb-chevron ms-Icon ms-Icon--ChevronRight"></i>'
-		    if (Custombreadcrumb != null) {
-		        breadCrumbNode.insertBefore(li, breadCrumbNode.childNodes[0]);
-		        getListTitle();//only supported on sharepoint online
-		        if (breadcrumbProperties.showFolders) {
-		            getFolders();
-		        }
-		    }
-		    if (site.get_serverRelativeUrl() !== currentWeb.get_serverRelativeUrl()) {
-		        RecursiveWeb(currentWeb.get_parentWeb().get_serverRelativeUrl())
-		    }
-		}, fail);
-    }
-
-
-    function RecursiveWeb(siteUrl) {
-        var Custombreadcrumb = document.getElementById('contentBox');
-        var breadCrumbNode = document.getElementById('breadcrumbSite');
-        var clientcontext = new SP.ClientContext(siteUrl);
-        var site = clientcontext.get_site();
-        var currentWeb = clientcontext.get_web();
-        clientcontext.load(currentWeb, 'ServerRelativeUrl', 'Title', 'ParentWeb', 'Url');
-        clientcontext.load(site, 'ServerRelativeUrl');
-        clientcontext.executeQueryAsync(function () {
-            if (site.get_serverRelativeUrl() !== currentWeb.get_serverRelativeUrl()) {
-                var li = document.createElement('li');
-                li.className = "ms-Breadcrumb-listItem";
-                li.innerHTML = '<a class="ms-Breadcrumb-itemLink" href="' + currentWeb.get_url() + '">' + currentWeb.get_title() + '</a><i class="ms-Breadcrumb-chevron ms-Icon ms-Icon--ChevronRight"></i>'
-                var Custombreadcrumb = document.getElementById('contentBox');
-                breadCrumbNode.insertBefore(li, breadCrumbNode.childNodes[0]);
-                RecursiveWeb(currentWeb.get_parentWeb().get_serverRelativeUrl())
-            } else {
-                var li = document.createElement('li');
-                li.className = "ms-Breadcrumb-listItem";
-                li.innerHTML = '<a class="ms-Breadcrumb-itemLink" href="' + currentWeb.get_url() + '">' + currentWeb.get_title() + '</a><i class="ms-Breadcrumb-chevron ms-Icon ms-Icon--ChevronRight"></i>'
-                breadCrumbNode.insertBefore(li, breadCrumbNode.childNodes[0]);
-            }
-        }, fail);
-    }
-
-    function fail() {
-        console.log('Unable to load SharePoint BreadCrumb');
-    }
-
-
-
-    //Feature Images
-    findImagesByRegexp();
-    function findImagesByRegexp() {
-        var images = Array.prototype.slice.call((document.getElementById('DeltaPlaceHolderMain') || document).getElementsByTagName('img'));
-        var length = images.length;
-        for (var i = 0; i < length; ++i) {
-            if (images[i].src.indexOf('breadcrumb.png') != -1) {
-                images[i].src = pathBreadCrumb + '/images/breadcrumb.png';
-            }
-        }
-    }
+        $("#titleAreaRow > .ms-breadcrumb-box").append(html);
+    });
 });
