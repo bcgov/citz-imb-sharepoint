7e9bdc2520e9f7bd11df54fa6fab6cd9dbaa0a1f
Toews <Scott.Toews@gov.bc.ca>
2019-06-27 08:40:36 -0700

    added load css function

--- Solutions/jslibraries/src/CITZ.IMB.SP.JSLibraries/Style Library/JSLibraries/js/citz.imb.sp.library.js
+++ Solutions/jslibraries/src/CITZ.IMB.SP.JSLibraries/Style Library/JSLibraries/js/citz.imb.sp.library.js
@@ -1,3 +1,14 @@
+$().ready(function () {
+    //put the css reference on the page
+    var jqeryUICssUrl = "https://" + window.location.hostname + _spPageContextInfo.siteServerRelativeUrl + "/Style%20Library/JSLibraries/css/jquery-ui.min.css";
+    var head = document.getElementsByTagName("head")[0];
+    var style = document.createElement("link");
+    style.type = "text/css";
+    style.rel = "stylesheet";
+    style.href = jqeryUICssUrl;
+    head.appendChild(style);
+});
+
 /**
  * Returns a promise of a JSON object containing SharePoint Groups having permissions on the current web
  *
